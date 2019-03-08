59c928855da94ca74a76b2807cacb22301021cd7
Lei Tai <onlytailei@hotmail.com>
2019-03-08 11:30:35 +0800

    update filter

--- Localization/extended_kalman_filter/src/extended_kalman_filter.cpp
+++ Localization/extended_kalman_filter/src/extended_kalman_filter.cpp
@@ -33,33 +33,47 @@ Eigen::Vector4f motion_model(Eigen::Vector4f x, Eigen::Vector2f u){
   return F_ * x + B_ * u;
 };
 
-Eigen::Matrix4f jacobF(Eigen::Vector4f x){
-  Eigen::Matrix4f jF_ = Eigen::Matrix::Identity();
-  float yaw = x
-  jF_<< 1, 0, 0, 0,
-        0, 1, 0, 0;
+Eigen::Matrix4f jacobF(Eigen::Vector4f x, Eigen::Vector2f u){
+  Eigen::Matrix4f jF_ = Eigen::Matrix4f::Identity();
+  float yaw = x(2);
+  float v = u(0);
+  jF_(0,2) = -DT * v * std::sin(yaw);
+  jF_(0,3) = DT * std::cos(yaw);
+  jF_(1,2) = DT * v * std::cos(yaw);
+  jF_(1,3) = DT * std::sin(yaw);
   return jF_;
-}
+};
 
 //observation mode H
 Eigen::Vector2f observation_model(Eigen::Vector4f x){
   Eigen::Matrix<float, 2, 4> H_;
   H_<< 1, 0, 0, 0,
        0, 1, 0, 0;
-
   return H_ * x;
-}
+};
 
-Eigen::Matrix<float, 4, 2> jacobH(Eigen::Vector4f x){
-  Eigen::Matrix<float, 4, 2> jH_;
+Eigen::Matrix<float, 2, 4> jacobH(Eigen::Vector4f x){
+  Eigen::Matrix<float, 2, 4> jH_;
   jH_<< 1, 0, 0, 0,
         0, 1, 0, 0;
   return jH_;
-}
+};
 
-void ekf_estimation(Eigen::Vector4f& xEst, Eigen::Matrix4f& PEst, Eigen::Vector2f z, Eigen::Vector2f u){
+void ekf_estimation(Eigen::Vector4f& xEst, Eigen::Matrix4f& PEst,
+  Eigen::Vector2f z, Eigen::Vector2f u,
+  Eigen::Matrix4f Q, Eigen::Matrix2f R){
     Eigen::Vector4f xPred = motion_model(xEst, u);
-}
+    Eigen::Matrix4f jF = jacobF(xPred, u);
+    Eigen::Matrix4f PPred = jF * PEst * jF.transpose() + Q;
+
+    Eigen::Matrix<float, 2, 4> jH = jacobH(xPred);
+    Eigen::Vector2f zPred = observation_model(xPred);
+    Eigen::Vector2f y = z - zPred;
+    Eigen::Matrix2f S = jH * PPred * jH.transpose() + R;
+    Eigen::Matrix<float, 4, 2> K = PPred * jH.transpose() * S.inverse();
+    xEst = xPred + K * y;
+    PEst = (Eigen::Matrix4f::Identity() - K * jH) * PPred;
+};
 
 
 int main(){
@@ -126,7 +140,8 @@ int main(){
     z(0) = xTrue(0) + gaussian_d(gen) * Rsim(0,0);
     z(1) = xTrue(1) + gaussian_d(gen) * Rsim(1,1);
 
-    ekf_estimation(xEst, PEst, z, ud);
+    ekf_estimation(xEst, PEst, z, ud, Q, R);
 
+    // TODO animation from images
   }
 }
