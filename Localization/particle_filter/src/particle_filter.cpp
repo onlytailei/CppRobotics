32ec7ace2602b033d9153815a0767498c91370df
Lei Tai <onlytailei@hotmail.com>
2019-03-12 14:37:10 +0800

    update particle filter

--- Localization/particle_filter/src/particle_filter.cpp
+++ Localization/particle_filter/src/particle_filter.cpp
@@ -5,11 +5,12 @@
 	> Created Time: Thu Mar  7 19:39:14 2019
  ************************************************************************/
 
-#include<iostream>
-#include<random>
-#include<math.h>
-#include<Eigen/Eigen>
-#include<opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <math.h>
+#include <Eigen/Eigen>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -65,23 +66,55 @@ Eigen::Matrix<float, 2, 4> jacobH(Eigen::Vector4f x){
   return jH_;
 };
 
-void ekf_estimation(Eigen::Vector4f& xEst, Eigen::Matrix4f& PEst,
-  Eigen::Vector2f z, Eigen::Vector2f u,
-  Eigen::Matrix4f Q, Eigen::Matrix2f R){
-    Eigen::Vector4f xPred = motion_model(xEst, u);
-    Eigen::Matrix4f jF = jacobF(xPred, u);
-    Eigen::Matrix4f PPred = jF * PEst * jF.transpose() + Q;
+// TODO gaussian likelihood
+// float gauss_likelihood(x, sigma):
+//     p = 1.0 / math.sqrt(2.0 * math.pi * sigma ** 2) * \
+//         math.exp(-x ** 2 / (2 * sigma ** 2))
+//
+//     return p
 
-    Eigen::Matrix<float, 2, 4> jH = jacobH(xPred);
-    Eigen::Vector2f zPred = observation_model(xPred);
-    Eigen::Vector2f y = z - zPred;
-    Eigen::Matrix2f S = jH * PPred * jH.transpose() + R;
-    Eigen::Matrix<float, 4, 2> K = PPred * jH.transpose() * S.inverse();
-    xEst = xPred + K * y;
-    PEst = (Eigen::Matrix4f::Identity() - K * jH) * PPred;
+void pf_localization(
+  Eigen::Matrix<float, 4, NP>& px, Eigen::Vector<float, NP>& pw,
+  Eigen::Vector4f& xEst, Eigen::Matrix4f& PEst,
+  std::vector<Eigen::RowVector3f> z, Eigen::Vector2f u){
+
+    for(int ip=0; ip<NP; i++){
+      Eigen::Vector4f x = px.col(ip);
+      w = pw(ip);
+
+      Eigen::Vector2f ud;
+
+      ud(0) = u(0) + gaussian_d(gen) * Qsim(0,0);
+      ud(1) = u(1) + gaussian_d(gen) * Qsim(1,1);
+
+      x = motion_model(x, ud);
+
+      for(int i=0; i<z.size(); i++){
+          float dx = x(0) - z[i](1);
+          float dy = x(1) - z[i](2);
+          float prez = std::sqrt(dx*dx + dy*dy);
+          float dz = prez - z[i](0);
+          // w = w * gauss_likelihood(dz, math.sqrt(Q[0, 0]))
+      }
+      // px[:, ip] = x[:, 0]
+      // pw[0, ip] = w
+    }
+
+    // Eigen::Vector4f xPred = motion_model(xEst, u);
+    // Eigen::Matrix4f jF = jacobF(xPred, u);
+    // Eigen::Matrix4f PPred = jF * PEst * jF.transpose() + Q;
+    //
+    // Eigen::Matrix<float, 2, 4> jH = jacobH(xPred);
+    // Eigen::Vector2f zPred = observation_model(xPred);
+    // Eigen::Vector2f y = z - zPred;
+    // Eigen::Matrix2f S = jH * PPred * jH.transpose() + R;
+    // Eigen::Matrix<float, 4, 2> K = PPred * jH.transpose() * S.inverse();
+    // xEst = xPred + K * y;
+    // PEst = (Eigen::Matrix4f::Identity() - K * jH) * PPred;
 };
 
-cv::Point2i cv_offset(Eigen::Vector2f e_p, int image_width=2000, int image_height=2000){
+cv::Point2i cv_offset(
+    Eigen::Vector2f e_p, int image_width=2000, int image_height=2000){
   cv::Point2i output;
   output.x = int(e_p(0) * 100) + image_width/2;
   output.y = image_height - int(e_p(1) * 100) - image_height/3;
@@ -98,6 +131,8 @@ int main(){
   // nosie control input
   Eigen::Vector2f ud;
 
+  // observation z
+  std::vector<Eigen::RowVector3f> z;
 
   // RFID remarks
   Eigen::Matrix<float, 4, 2> RFID;
@@ -161,9 +196,7 @@ int main(){
     xTrue = motion_model(xTrue, u);
     xDR = motion_model(xDR, ud);
 
-    // observation z
-    Eigen::Matrix3cf z;
-
+    z.clear();
     for(int i=0; i<NP; i++){
       float dx = xTrue(0) - RFID(i, 0);
       float dy = xTrue(1) - RFID(i, 1);
@@ -172,24 +205,28 @@ int main(){
         float dn = d + gaussian_d(gen) * Qsim(0);
         Eigen::RowVector3f zi;
         zi<<dn, RFID(i, 0), RFID(i, 1);
-        z =
+        z.push_back(zi);
       }
-
     }
 
-    z(0) = xTrue(0) + gaussian_d(gen) * Rsim(0,0);
-    z(1) = xTrue(1) + gaussian_d(gen) * Rsim(1,1);
-
-    ekf_estimation(xEst, PEst, z, ud, Q, R);
+    // TODO pf localization
 
     // blue estimation
-    cv::circle(bg, cv_offset(xEst.head(2), bg.cols, bg.rows), 10, cv::Scalar(255,0,0), -1);
+    cv::circle(bg, cv_offset(xEst.head(2), bg.cols, bg.rows),
+               10, cv::Scalar(255,0,0), -1);
+
     // green groundtruth
-    cv::circle(bg, cv_offset(xTrue.head(2), bg.cols, bg.rows), 10, cv::Scalar(0,255,0), -1);
+    cv::circle(bg, cv_offset(xTrue.head(2), bg.cols, bg.rows),
+               10, cv::Scalar(0,255,0), -1);
+
     // black dead reckoning
-    cv::circle(bg, cv_offset(xDR.head(2), bg.cols, bg.rows), 10, cv::Scalar(0, 0, 0), -1);
+    cv::circle(bg, cv_offset(xDR.head(2), bg.cols, bg.rows),
+               10, cv::Scalar(0, 0, 0), -1);
+
     // red observation
-    cv::circle(bg, cv_offset(z, bg.cols, bg.rows), 10, cv::Scalar(0, 0, 255), -1);
+    cv::circle(bg, cv_offset(z, bg.cols, bg.rows),
+               10, cv::Scalar(0, 0, 255), -1);
+
     cv::imshow("ekf", bg);
     cv::waitKey(5);
 
