f7ed7422c48b857ac16af9bbee41c97cfb8227b5
Lei Tai <onlytailei@hotmail.com>
2019-03-21 16:08:27 +0800

    fix name and prepare mptg

---
+++ PathPlanning/model_predictive_trajectory_generator/include/motion_model.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: motion_model.h
+	> Author: TAI Lei
+	> Mail: ltai@ust.hk
+	> Created Time: Thu Mar 21 16:06:07 2019
+ ************************************************************************/
+
+#ifndef _MOTION_MODEL_H
+#define _MOTION_MODEL_H
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+#endif
