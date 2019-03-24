7c7ce7c8eb41811f9784d0bf9713008c2b842094
Lei Tai <onlytailei@hotmail.com>
2019-03-24 21:48:09 +0800

    update target robots

--- PathPlanning/model_predictive_trajectory_generator/include/motion_model.h
+++ PathPlanning/model_predictive_trajectory_generator/include/motion_model.h
@@ -10,29 +10,68 @@
 
 #include <iostream>
 #include <cmath>
-#include <boost/math/interpolators/cubic_b_spline.hpp>
+#include <cfenv>
+// #include <boost/math/interpolators/cubic_b_spline.hpp>
 
-#define PI 3.141592653
-using TrajState = std::array<float, 3>;
+// #define PI 3.141592
+#define YAW_P2P(angle) std::fmod((angle+(float)M_PI), 2*M_PI)-M_PI
+
+struct Parameter{
+  float distance=0;
+  // at least 5 number
+  std::array<float, 3> steering_sequence{{0,0,0}};
+  Parameter(float distance_, std::array<float, 3> steering_sequence_){
+    distance = distance_;
+    steering_sequence = steering_sequence_;
+  };
+};
+
+
+struct State{
+  float x;
+  float y;
+  float yaw;
+  float v;
+  State(float x_, float y_, float yaw_, float v_){
+    x = x_;
+    y = y_;
+    yaw = yaw_;
+    v = v_;
+  };
+};
+
+struct TrajState{
+  float x;
+  float y;
+  float yaw;
+  TrajState(float x_, float y_, float yaw_){
+    x = x_;
+    y = y_;
+    yaw = yaw_;
+  };
+};
+
 using Traj = std::vector<TrajState>;
 
-class State{
-  public:
-    float x;
-    float y;
-    float yaw;
-    float v;
+std::vector<float> quadrati_interpolation(
+    std::array<float, 3> x, std::array<float, 3> y){
+    Eigen::Matrix3f A;
+    Eigen::Vector3f Y;
+    A<< std::pow(x[0], 2), x[0], 1,
+        std::pow(x[1], 2), x[1], 1,
+        std::pow(x[2], 2), x[2], 1;
+    Y<<y[0], y[1], y[2];
 
-    State();
-    State(float x_, float y_, float yaw_, float v_);
-};
+    Eigen::Vector3f result = A.inverse() * Y;
+    float* result_data = result.data();
+    std::vector<float> result_array(result_data, result_data+3);
+    return result_array;
+}
 
-State::State(float x_, float y_, float yaw_, float v_){
-  x = x_;
-  y = y_;
-  yaw = yaw_;
-  v = v_;
-};
+float interp_refer(std::vector<float> para, float x){
+    return para[0] * x * x + para[1] * x + para[2];
+}
+
 
 class MotionModel{
   public:
@@ -43,10 +82,8 @@ class MotionModel{
     MotionModel(float base_l_, float ds_, State state_):
       base_l(base_l_), ds(ds_), state(state_){};
     void update(float v_, float delta, float dt);
-    Traj generate_trajectory(float, std::vector<float>);
-
-    void generate_last_state(float, std::Vector<float>);
-  // private:
+    Traj generate_trajectory(Parameter);
+    TrajState generate_last_state(Parameter);
 
 };
 
@@ -55,51 +92,49 @@ void MotionModel::update(float v_, float delta, float dt){
     state.x = state.x + state.v * std::cos(state.yaw) * dt;
     state.y = state.y + state.v * std::sin(state.yaw) * dt;
     state.yaw = state.yaw + state.v / base_l * std::tan(delta) * dt;
-    if (state.yaw > PI){
-      state.yaw = state.yaw - 2 * PI;
-    }
+    state.yaw = YAW_P2P(state.yaw);
 };
 
-void MotionModel::generate_trajectory(float s, std::vector<float> kk){
-  // NOTE boost cubic b ask for at least 5 points
-  // s: distance
-  // kk: steering of becycle model as specific position
-  float n = s / ds;
-  float horizon = s / state.v;
+Traj MotionModel::generate_trajectory(Parameter p){
+  float n =  p.distance / ds;
+  float horizon = p.distance / state.v;
 
-  boost::math::cubic_b_spline<float> spline(
-    kk.data(), kk.size(), 0, horizon/kk.size());
-
-  Traj output;
-
-  for(float i=0.0; i<horizon; i+=horizon/n){
-      float kp = spline(i);
-      update(state.v, kp, horizon/n);
-      std::array<float, 3> xyyaw{state.x state.y, state.z};
-      output.push_back(xyyaw);
-    }
-    return output;
-}
-
-void MotionModel::generate_last_state(float s, std::vector<float> kk){
-  // NOTE boost cubic b ask for at least 5 points
-  // s: distance
-  // kk: steering of becycle model as specific position
-  float n = s / ds;
-  float horizon = s / state.v;
-
-  boost::math::cubic_b_spline<float> spline(
-    kk.data(), kk.size(), 0, horizon/kk.size());
+  // boost::math::cubic_b_spline<float> spline(
+  //   p.steering_sequence.data(), p.steering_sequence.size(),
+  //   0, horizon/p.steering_sequence.size());
+  std::vector<float> spline = quadrati_interpolation(
+    {0, horizon/2, horizon},
+    p.steering_sequence);
 
   Traj output;
 
   for(float i=0.0; i<horizon; i+=horizon/n){
-      float kp = spline(i);
+      float kp = interp_refer(spline, i);
       update(state.v, kp, horizon/n);
-      Trajstate xyyaw{state.x state.y, state.z};
+      TrajState xyyaw{state.x, state.y, state.yaw};
       output.push_back(xyyaw);
-    }
-    return output;
+  }
+  return output;
 }
+
+TrajState MotionModel::generate_last_state(Parameter p){
+  float n = p.distance / ds;
+  float horizon = p.distance / state.v;
+
+  // boost::math::cubic_b_spline<float> spline(
+  //   p.steering_sequence.data(), p.steering_sequence.size(),
+  //   0, horizon/p.steering_sequence.size());
+  std::vector<float> spline = quadrati_interpolation(
+    {0, horizon/2, horizon},
+    p.steering_sequence);
+
+  for(float i=0.0; i<horizon; i+=horizon/n){
+      float kp = interp_refer(spline, i);
+      update(state.v, kp, horizon/n);
+  }
+  return TrajState{state.x, state.y, state.yaw};
+}
+
+
 
 #endif
--- PathPlanning/model_predictive_trajectory_generator/src/model_predictive_trajectory_generator.cpp
+++ PathPlanning/model_predictive_trajectory_generator/src/model_predictive_trajectory_generator.cpp
@@ -12,14 +12,135 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <Eigen/Eigen>
 #include "motion_model.h"
 
 #define L 1.0
 #define DS 0.1
 #define CONST_V 3.0  // use a const linear velocity here
 
+
+TrajState calc_diff(TrajState target_, TrajState traj_state_){
+    float yaw_ = target_.yaw - traj_state_.yaw;
+    return {target_.x - traj_state_.x,
+            target_.y - traj_state_.y,
+            YAW_P2P(yaw_)};
+};
+
+Eigen::Vector3f error_vector(MotionModel m_model_, Parameter p, TrajState target_){
+    TrajState last_state = m_model_.generate_last_state(p);
+    TrajState d_error = calc_diff(target_, last_state);
+    Eigen::Vector3f d_vct;
+    d_vct<< d_error.x , d_error.y, d_error.yaw;
+    return d_vct;
+
+}
+
+Eigen::Matrix3f calc_J(MotionModel m_model_,
+                       TrajState target_,
+                       Parameter p,
+                       std::vector<float> h){
+
+    Parameter p00 = p;
+    p00.distance = p.distance + h[0];
+    Eigen::Vector3f d00_vct = error_vector(m_model_, p00, target_);
+    Parameter p01 = p;
+    p01.distance = p.distance - h[0];
+    Eigen::Vector3f d01_vct = error_vector(m_model_, p01, target_);
+    Eigen::Vector3f d0 = (d00_vct - d01_vct) / (2.0 * h[0]);
+
+    Parameter p10 = p;
+    p10.steering_sequence[1] = p.steering_sequence[1] + h[1];
+    Eigen::Vector3f d10_vct = error_vector(m_model_, p10, target_);
+    Parameter p11 = p;
+    p11.steering_sequence[1] = p.steering_sequence[1] - h[1];
+    Eigen::Vector3f d11_vct = error_vector(m_model_, p11, target_);
+    Eigen::Vector3f d1 = (d10_vct - d11_vct) / (2.0 * h[1]);
+
+    Parameter p20 = p;
+    p10.steering_sequence[2] = p.steering_sequence[2] + h[2];
+    Eigen::Vector3f d20_vct = error_vector(m_model_, p20, target_);
+    Parameter p21 = p;
+    p11.steering_sequence[2] = p.steering_sequence[2] - h[2];
+    Eigen::Vector3f d21_vct = error_vector(m_model_, p21, target_);
+    Eigen::Vector3f d2 = (d20_vct - d21_vct) / (2.0 * h[2]);
+
+    Eigen::Matrix3f J;
+    J<<d0, d1, d2;
+    return J;
+
+  }
+
+
+float selection_learning_param(
+    MotionModel motion_model, Eigen::Vector3f dp, Parameter p, TrajState target){
+    float mincost = std::numeric_limits<float>::max();
+    float mina = 1.0;
+    float maxa = 2.0;
+    float da = 0.5;
+
+    for(float a=mina; a<maxa; a+=da){
+        Parameter new_p = p;
+        new_p.distance += a * dp[0];
+        new_p.steering_sequence[1] += a * dp[1];
+        new_p.steering_sequence[2] += a * dp[2];
+        TrajState laststate = motion_model.generate_last_state(new_p);
+        TrajState dc = calc_diff(target, laststate);
+        float cost = std::sqrt(std::pow(dc.x, 2)+std::pow(dc.y, 2)+std::pow(dc.yaw, 2));
+
+        if ((cost <= mincost) && (a != 0.0)){
+            mina = a;
+            mincost = cost;
+        }
+    }
+
+    return mina;
+}
+
+
+Traj optimize_trajectory(MotionModel m_model,
+                         TrajState target_state,
+                         Parameter p_,
+                         int max_iter,
+                         float cost_th_,
+                         std::vector<float> h_step_){
+  Traj sample_traj;
+  for(int i=0; i<max_iter; i++){
+    sample_traj = m_model.generate_trajectory(p_);
+    TrajState dc = calc_diff(target_state, sample_traj.back());
+    Eigen::Vector3f dc_vct;
+    dc_vct<< dc.x , dc.y, dc.yaw;
+    float cost = std::sqrt(std::pow(dc.x, 2)+std::pow(dc.y, 2)+std::pow(dc.yaw, 2));
+    std::cout << "start gene" << std::endl;
+    if (cost < cost_th_){
+      std::cout << "find the traj under the cost threshold" << std::endl;
+      break;
+    }
+
+    Eigen::Matrix3f J = calc_J(m_model, target_state, p_, h_step_);
+    Eigen::Vector3f dp = J.inverse() * dc_vct;
+
+    float alpha = selection_learning_param(m_model, dp, p_, target_state);
+
+    p_.distance += alpha * dp[0];
+    p_.steering_sequence[1] += alpha * dp[1];
+    p_.steering_sequence[2] += alpha * dp[2];
+  }
+
+  return sample_traj;
+};
+
+
 int main(){
-  State state(0, 0, 0, CONST_V);
-  MotionModel motion_model(L, DS, state);
-  motion_model.generate_trajectory(6, {0.0, 0.1, 0.2, 0.3, 0.4});
+  State init_state(0, 0, 0, CONST_V);
+  TrajState target(5, 2.0, 0);
+  MotionModel m_model(L, DS, init_state);
+
+  Parameter init_p(6, {{0,0,0}});
+  float cost_th = 0.1;
+  std::vector<float> h_step{0.5, 0.02, 0.02};
+  int max_iter = 5;
+
+  Traj final_traj = optimize_trajectory(m_model, target, init_p,
+                         max_iter, cost_th, h_step);
 }
